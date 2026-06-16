d777ebb6b8d556de8dd79d4c7ed6003d7af9c195
Mithil Girish <mithilgirish@users.noreply.github.com>
2026-06-16 08:44:32 +0000

    traz 0.1.0

---
+++ Formula/traz.rb
@@ -0,0 +1,58 @@
+class Traz < Formula
+  desc "A local-first engineering memory layer and MCP server for AI coding tools"
+  homepage "https://traz.mithilgirish.dev"
+  version "0.1.0"
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/mithilgirish/traz/releases/download/v0.1.0/traz-aarch64-apple-darwin.tar.xz"
+    sha256 "83db95bbe3331733867f45f556dd126ec93d94014b4fd170c1491f2768b211c6"
+  end
+  if OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/mithilgirish/traz/releases/download/v0.1.0/traz-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "d4209f6b3d6e621b782d49aec449ee5c60a217c0374cfbc9da38db6f8c255389"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/mithilgirish/traz/releases/download/v0.1.0/traz-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "59ba190408761a465d510dde54573409a327abdcdba1957c9a9aec6d52a07aa0"
+    end
+  end
+  license "MIT"
+
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin":      {},
+    "aarch64-unknown-linux-gnu": {},
+    "x86_64-pc-windows-gnu":     {},
+    "x86_64-unknown-linux-gnu":  {},
+  }.freeze
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
+
+  def install
+    bin.install "traz" if OS.mac? && Hardware::CPU.arm?
+    bin.install "traz" if OS.linux? && Hardware::CPU.arm?
+    bin.install "traz" if OS.linux? && Hardware::CPU.intel?
+
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  end
+end
